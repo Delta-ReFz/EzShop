a66a5a65c85f29de5aa9ecb03dc40ca487da23f2
Delta-ReFz <el-mehdi.oukid.1@ens.etsmtl.ca>
2024-12-08 02:44:21 -0500

    Lets create a data base MongoDB

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ import express from  'express';
 
 const app = express();
 
+app.get("/products", (req,res) => {
+    res.send("Server is ready 123");
+})
+
 app.listen(5000, () => {
     console.log('Server started at http://localhost:5000');
 });
