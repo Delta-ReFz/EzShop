fc068d86a3a27854a4f174009930d45c1cca2d7c
Delta-ReFz <el-mehdi.oukid.1@ens.etsmtl.ca>
2024-12-10 22:34:20 -0500

    GET & DELETE works

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-import express from  'express';
+import express from 'express';
 import dotenv from "dotenv";
 import { connectDB } from './config/db.js';
 import Product from './model/product.model.js';
@@ -9,24 +9,47 @@ const app = express();
 
 app.use(express.json()); // Allows us to accept JSON data in the body ==> req.body
 
-app.post("/api/products", async (req,res) => {
+app.get("/api/products", async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json({succes: true, data: products});
+    } catch (error) {
+        console.log("error in fetching products:", error.message);
+        res.status(500).json({succes: false, message: "Server Error"});
+        
+    }
+})
+
+app.post("/api/products", async (req, res) => {
 
     const product = req.body; // user will send this data
 
-    if(!product.name || !product.price || !product.image) {
-        return res.status(400).json({success:false, message: "Please enter all fields"});
+    if (!product.name || !product.price || !product.image) {
+        return res.status(400).json({ success: false, message: "Please enter all fields" });
     }
 
     const newProduct = new Product(product);
 
-    try{
+    try {
         await newProduct.save();
-        res.status(201).json({success: true, data: newProduct});
-    } catch(error) {
+        res.status(201).json({ success: true, data: newProduct });
+    } catch (error) {
 
         console.error("Error in Create product", error.message);
-        res.status(500).json({succes:false, message:"Server Error"});
+        res.status(500).json({ succes: false, message: "Server Error" });
 
+    }
+});
+
+app.delete("/api/products/:id", async (req, res) => {
+    const {id} = req.params;
+    
+    try {
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({succes: true, message: "Product deleted"});
+    } catch (error) {
+        console.log("error in deleting product:", error.message);
+        res.status(404).json({succes: false, message: "Product not found"});
     }
 });
 
