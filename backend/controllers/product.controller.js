651924df1ec776d793633f50944f32d2ef156b7e
Delta-ReFz <el-mehdi.oukid.1@ens.etsmtl.ca>
2024-12-12 17:50:01 -0500

    Upgrades backend

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,4 +1,5 @@
 import Product from "../model/product.model.js";
+import mongoose from "mongoose";
 
 //GET
 export const getProducts = async (req, res) => {
@@ -56,11 +57,15 @@ export const updateProduct = async (req, res) => {
 export const deleteProduct = async (req, res) => {
     const {id} = req.params;
     
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({succes:false, message:"Invalid Product Id"});
+    }
+
     try {
         await Product.findByIdAndDelete(id);
         res.status(200).json({succes: true, message: "Product deleted"});
     } catch (error) {
         console.log("error in deleting product:", error.message);
-        res.status(404).json({succes: false, message: "Product not found"});
+        res.status(500).json({succes: false, message: "Server Error"});
     }
 };
--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,19 @@
 import express from 'express';
 import dotenv from "dotenv";
 import { connectDB } from './config/db.js';
-import Product from './model/product.model.js';
-import mongoose from 'mongoose';
 import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT
 
 app.use(express.json()); // Allows us to accept JSON data in the body ==> req.body
 
 app.use("/api/products", productRoutes);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     connectDB();
-    console.log('Server started at http://localhost:5000');
+    console.log('Server started at http://localhost:'+ PORT);
 });
 
