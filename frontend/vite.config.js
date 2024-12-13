eb8c24f58dbb507aae8ef6ab4b0845408f6b2b79
Delta-ReFz <el-mehdi.oukid.1@ens.etsmtl.ca>
2024-12-13 02:43:07 -0500

    Started the frontend

---
+++ frontend/vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths'; // Import du plugin
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),          // Plugin React
+    tsconfigPaths(),  // Plugin pour synchroniser les alias
+  ],
+});
