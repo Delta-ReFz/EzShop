020adea0bb1966ed330f26f1e213efedb6525210
Delta-ReFz <el-mehdi.oukid.1@ens.etsmtl.ca>
2024-12-13 13:15:45 -0500

    home page + light/dark mode

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from 'react'
-import { Button } from "@/components/ui/button"
+import { Box } from '@chakra-ui/react'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage';
+import CreatePage from './pages/CreatePage';
+import Navbar from './components/Navbar';
+import { useColorModeValue } from './components/ui/color-mode';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <Button>Click me</Button>
-      <Button>Click me</Button>
-    </>
+    <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
+      <Navbar />
+     <Routes>
+      <Route path='/' element={<HomePage />}/>
+      <Route path='/create' element={<CreatePage />}/>
+     </Routes>
+    </Box>
   )
 }
 
