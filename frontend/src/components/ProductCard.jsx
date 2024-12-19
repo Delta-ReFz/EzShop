d766b9016419330201dc93c08b7f9a021f558158
Delta-ReFz <el-mehdi.oukid.1@ens.etsmtl.ca>
2024-12-18 23:59:26 -0500

    modal

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,10 +1,12 @@
-import { Box, Heading, HStack, Icon, IconButton, Image, Text } from '@chakra-ui/react';
+import { Box, Heading, HStack, IconButton, Image, Text } from '@chakra-ui/react';
 import React from 'react';
 import { useColorModeValue } from './ui/color-mode';
 import { FaEdit } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
 import { useProductStore } from '@/store/product';
 import { toaster } from "@/components/ui/toaster";
+import { Button } from "@/components/ui/button"
+import {DialogActionTrigger, DialogBody,DialogCloseTrigger,DialogContent,DialogFooter,DialogHeader,DialogRoot,DialogTitle,DialogTrigger,} from "@/components/ui/dialog"
 
 const ProductCard = ({ product }) => {
     const TextColor = useColorModeValue("gray.200", "gray.600");
@@ -50,6 +52,32 @@ const ProductCard = ({ product }) => {
                     <IconButton onClick={() => handleDeleteProduct(product._id)} bgColor={'red.400'}> <MdDelete /> </IconButton>
                 </HStack>
             </Box>
+
+            <DialogRoot>
+                <DialogTrigger asChild>
+                    <Button variant="outline" size="sm">
+                        Open Dialog
+                    </Button>
+                </DialogTrigger>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>Dialog Title</DialogTitle>
+                    </DialogHeader>
+                    <DialogBody>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+                            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
+                    </DialogBody>
+                    <DialogFooter>
+                        <DialogActionTrigger asChild>
+                            <Button variant="outline">Cancel</Button>
+                        </DialogActionTrigger>
+                        <Button>Save</Button>
+                    </DialogFooter>
+                    <DialogCloseTrigger />
+                </DialogContent>
+            </DialogRoot>
         </Box>
     );
 };
